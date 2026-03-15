7b9864156729b256692f81ac984879c2c3a7922c
Kawahara <My.favorite.is.energy@gmail.com>
2026-03-15 21:58:06 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ rm -rf dist
 mkdir dist
 
 cp index.html dist/
-cp main.css dist/
-cp reset.css dist/
+cp devlogs.html dist/
+cp posts.html dist/
+cp products.html dist/
+
+
+cp -r css/ dist/css/
 cp -r images/ dist/images/
 cp -r fonts/ dist/fonts/
 
